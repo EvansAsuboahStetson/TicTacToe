6583ea8037b9a8b72965cc458ce8e1b35755f688
Evans Asuboah <easuboah@stetson.edu>
2023-04-08 14:15:21 -0400

    TicTac Toe

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './Game';
+
+function App() {
+  return (
+    <div className="App">
+    <Board/>
+    </div>
+  );
+}
+
+export default App;
